24c4e1c0ef350ac4488e751a8c0d05d1961d1494
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2022-11-28 14:55:03 +0900

    dddd

--- lib/parts/header.dart
+++ lib/parts/header.dart
@@ -4,8 +4,9 @@ import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 import '../login/login.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Header extends StatefulWidget with PreferredSizeWidget {
-    const Header({
+  const Header({
     Key? key,
   }) : super(key: key);
 
@@ -16,97 +17,112 @@ class Header extends StatefulWidget with PreferredSizeWidget {
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
 
-
 class _Header extends State<Header> {
   bool _searchBoolean = false;
   final User? user = FirebaseAuth.instance.currentUser;
+  Future<String> fetchCoin() async {
+    final docs = await FirebaseFirestore.instance
+        .collection('T01_Person')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    final data = docs.exists ? docs.data() : null;
+    print('out = ' + data!['T01_AhaCoin'].toString());
+    return data['T01_AhaCoin'].toString();
+  }
+
+  String _coin = "2";
+  int i = 0;
   // final docs = FirebaseFirestore.instance.collection('T01_Person').doc(FirebaseAuth.instance.currentUser!.uid).get();
   //final coin = fetchCoin();
 
   @override
   Widget build(BuildContext context) {
     // final docSnapshot = FirebaseFirestore.instance.collection('T01_Person').doc(user!.uid).get();
-    Future<String> fetchCoin() async {
-      final docs = await FirebaseFirestore.instance.collection('T01_Person').doc(user!.uid).get();
-      final data = docs.exists ? docs.data() : null;
-      print('out = '+ data!['T01_AhaCoin'].toString());
-      return data['T01_AhaCoin'].toString();
-    }
-    
-    return AppBar(
-      
-      title: !_searchBoolean ? Text('アハマッチ!') : _searchTextField(),
-      actions: !_searchBoolean
-        ? [
-          IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {
-            setState(() {
-              _searchBoolean = true;
-            });
-          }),
-          TextButton.icon(
-              icon: const Icon(Icons.monetization_on),
-              label: Text(fetchCoin().then((value) => value).toString()),
-              style: TextButton.styleFrom(
-                      foregroundColor: Colors.white,
-                    ),
-              onPressed: () {},
-          ),
-          IconButton(
-          icon: Icon(Icons.notifications),
-          onPressed: () {
-            setState(() {
-
-            });
-          }),
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              // ログアウト処理
-              // 内部で保持しているログイン情報等が初期化される
-              // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
-              await FirebaseAuth.instance.signOut();
-              // ログイン画面に遷移＋チャット画面を破棄
-              await Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) {
-                  return UserLogin();
-                }),
-              );
-            },
-          ),
-        ] 
-        : [
-          IconButton(
-            icon: Icon(Icons.clear),
-            onPressed: () {
-              setState(() {
-                _searchBoolean = false;
-              });
-            }
-          )
-        ]
+    fetchCoin().then(
+      (value) {
+        print(value);
+        // _coin = value.toString();
+        if (i == 0) {
+          setState(() {
+            _coin = value.toString();
+            i++;
+          });
+        }
+      },
     );
+    return AppBar(
+        title: !_searchBoolean ? Text('アハマッチ!') : _searchTextField(),
+        actions: !_searchBoolean
+            ? [
+                IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {
+                        _searchBoolean = true;
+                      });
+                    }),
+                TextButton.icon(
+                  icon: const Icon(Icons.monetization_on),
+                  label: Text(_coin),
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                    icon: Icon(Icons.notifications),
+                    onPressed: () {
+                      setState(() {});
+                    }),
+                IconButton(
+                  icon: Icon(Icons.logout),
+                  onPressed: () async {
+                    // ログアウト処理
+                    // 内部で保持しているログイン情報等が初期化される
+                    // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
+                    await FirebaseAuth.instance.signOut();
+                    // ログイン画面に遷移＋チャット画面を破棄
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) {
+                        return UserLogin();
+                      }),
+                    );
+                  },
+                ),
+              ]
+            : [
+                IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      setState(() {
+                        _searchBoolean = false;
+                      });
+                    })
+              ]);
   }
 
-  Widget _searchTextField() { //検索バーの見た目
+  Widget _searchTextField() {
+    //検索バーの見た目
     return TextField(
       autofocus: true, //TextFieldが表示されるときにフォーカスする（キーボードを表示する）
       cursorColor: Colors.white, //カーソルの色
-      style: TextStyle( //テキストのスタイル
+      style: TextStyle(
+        //テキストのスタイル
         color: Colors.white,
         fontSize: 20,
       ),
       textInputAction: TextInputAction.search, //キーボードのアクションボタンを指定
-      decoration: InputDecoration( //TextFiledのスタイル
-        enabledBorder: UnderlineInputBorder( //デフォルトのTextFieldの枠線
-          borderSide: BorderSide(color: Colors.white)
-        ),
-        focusedBorder: UnderlineInputBorder( //TextFieldにフォーカス時の枠線
-          borderSide: BorderSide(color: Colors.white)
-        ),
+      decoration: InputDecoration(
+        //TextFiledのスタイル
+        enabledBorder: UnderlineInputBorder(
+            //デフォルトのTextFieldの枠線
+            borderSide: BorderSide(color: Colors.white)),
+        focusedBorder: UnderlineInputBorder(
+            //TextFieldにフォーカス時の枠線
+            borderSide: BorderSide(color: Colors.white)),
         hintText: 'Search', //何も入力してないときに表示されるテキスト
-        hintStyle: TextStyle( //hintTextのスタイル
+        hintStyle: TextStyle(
+          //hintTextのスタイル
           color: Colors.white60,
           fontSize: 20,
         ),
---
+++ lib/post/uploadPost.dart
@@ -0,0 +1,2 @@
+import 'package:ahamatch/home/home.dart';
+import 'package:ahamatch/main.dart';
