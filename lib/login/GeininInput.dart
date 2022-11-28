a08b46c1cf2b9de566f39e2771090a43dc3aa031
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2022-11-28 11:44:56 +0900

    inputs

--- lib/login/GeininInput.dart
+++ lib/login/GeininInput.dart
@@ -23,42 +23,106 @@ class _GeininInput extends State<GeininInput> {
   final _scaffoldKey = GlobalKey();
 
   void _upload(int type, String name, String id) async {
-    await FirebaseFirestore.instance
-        .collection('T01_Person') // コレクションID
-        .doc(user!.uid) // ドキュメントID
-        .set({
-      // 'T01_AhaPoint': 0,
-      // 'T01_Kind': type,
-      // "T01_Subscribe": false,
-      // "T01_UserId": userid,
-      // "T01_DisplayName": name,
-      // 'T01_Create': Timestamp.fromDate(DateTime.now())
-    });
+    final document = await FirebaseFirestore.instance
+        .collection("T01_Person")
+        .doc(user!.uid)
+        .get();
+    final data = document.exists ? document.data() : null;
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(data!["T01_DisplayName"] + "ffffff"),
+      ),
+    );
+    //  var doclist = document.docs;
+
+    // await FirebaseFirestore.instance
+    //     .collection('T02_Person') // コレクションID
+    //     .doc(user!.uid) // ドキュメントID
+    //     .set({
+    //   'T02_GeininId': 0,
+    //   'T02_UnitName': type,
+    //   "T02_PartnerRecruit": false,
+    //   "T02_Tags": user,
+    //   "T02_describe": name,
+    //   'T02_Create': Timestamp.fromDate(DateTime.now()),
+    //   "T02_production": user,
+    // });
   }
 
   String name = "名無し";
   String id = "xxxxxx";
 
-  int? isSelectedItem = 2;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      body: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                try {} catch (e) {}
-              },
-              child: const Text('送信'),
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Container(
+              height: double.infinity,
+              alignment: Alignment.topCenter,
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('T01_Person')
+                    // .orderBy('createdAt')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text('エラーが発生しました');
+                  }
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  final list = snapshot.requireData.docs
+                      .map<String>((DocumentSnapshot document) {
+                    final documentData =
+                        document.data()! as Map<String, dynamic>;
+                    return documentData['T01_DisplayName']! as String;
+                  }).toList();
+                  _upload(2, name, id);
+                  final reverseList = list.reversed.toList();
+
+                  return ListView.builder(
+                    itemCount: reverseList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Center(
+                        child: Text(
+                          reverseList[index],
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
-          ],
-        ),
-      ),
+          ),
+        ],
+      )),
     );
   }
 }
+
+
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     key: _scaffoldKey,
+  //     body: Form(
+  //       key: _formKey,
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //         children: [
+  //           ElevatedButton(
+  //             onPressed: () async {
+  //               try {} catch (e) {}
+  //             },
+  //             child: const Text('送信'),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+// }
